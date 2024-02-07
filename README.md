4e20c0229a166afb71d34f982490ebaf6f3c978e
Jayant GUPTA <jayantgupta5582@gmail.com>
2024-02-07 20:19:19 +0530

    M1 First Commit

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/M9yOg1uw)
+Steps to Run in Virtual Environment:
+
+    Navigate to the directory where isslab3.py exists in the terminal.
+
+    In the terminal, execute the following commands:
+
+    ```bash
+
+    python3 -m venv venv
+    
+    ./run.sh
+    ```
+
+    Details about the website being opened will be displayed in the terminal.
+
+    To access the site, type in the link provided in the terminal. For example, it might show http://127.0.0.1:5000.
+
+    Happy logging!
+
+
+Additional Information:
+
+If Flask is not working, you can refer to this link for installation on Ubuntu 20.04: How to Install Flask on Ubuntu 20.04.
+
+Also, once flask is set up, just run run.sh in the directory.
+
+Assumption:
+
+-Login page is in home page
+-Images can be uploaded by drag and drop as well as selected and multiple images are suppoerted
+-There is checkbox on the top corner of the photos which allows user to order the photos for the video
+-Clicking on it allots it in
+-There is a drop down menu on the other top corner (threee dots) with which you can specify duration and transition for each image
+-The photo selection works like ms lens ordering, clicking selects it and allots the highest number+1
+-Deselecting it decrements the images higher than it by one
+-You can add multiple bg music to the video
+-Play pause can be toggled with spacebar
+-Rewind and fastforward with arrow keys
+-Right clicking on video gives you multiple options such as playback speed etc
+-Functions such as download will be added later
+-using username: admin password:admin leads to admin screen
+-We are assuming the flask server is up and running and the exchange token is done
+-Download and resolution button will be made functional later
+-For now, only admin pages work. Login and register will be implemented later on. Security protocols must be observed.
+
---
+++ isslab3.py
@@ -0,0 +1,236 @@
+import json
+from flask import Flask, render_template_string, request, redirect, url_for, session, render_template
+from werkzeug.security import generate_password_hash, check_password_hash
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'qeervrvpnmakswplrj562mcrorl104k5n'  # Replace with a strong secret key
+app.config['SESSION_COOKIE_PERMANENT'] = True  # Enable persistent cookies
+import os
+
+if not os.path.exists('users.txt'):
+    with open('users.txt', 'w'):
+        pass
+
+@app.route('/main')
+def main():
+    return render_template('http://127.0.0.1:3000/main.html')
+
+@app.route('/hardcode')
+def hardcode():
+    return render_template('http://127.0.0.1:3000/hardcode.html')
+
+    
+@app.route('/')  #home page route
+def home():
+    if 'user_email' in session:
+        return 'Welcome back, ' + session['user_email'] + '!'
+    else:
+        return redirect(url_for('login'))
+@app.route('/register', methods=['GET', 'POST']) #home page redirects to register which accepts get and post requests, hashes password and also checks for duplicate email
+def register():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        hashed_password = generate_password_hash(password)
+        user_data = {
+            'name': name,
+            'email': email,
+            'password': hashed_password
+        }
+        with open('users.txt', 'r') as infile:
+            users = infile.readlines()
+            for user_json in users:
+                existing_user_data = json.loads(user_json)
+                if existing_user_data['email'] == email:
+                    return 'Email already registered.'
+
+        with open('users.txt', 'a') as outfile:
+            json.dump(user_data, outfile)
+            outfile.write('\n')
+        return 'Registration successful.'
+
+    register_form_html = """
+    
+         <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form Animation CSS | Codehal</title>
+    <link rel="stylesheet" href="{{ url_for('static', filename='style.css') }}">
+</head>
+
+<body>
+
+    <div class="login-light"></div>
+    <div class="login-box">
+        <form action="/login" method="post">
+            <input type="checkbox" class="input-check" id="input-check">
+            <label for="input-check" class="toggle">
+                <span class="text off">off</span>
+                <span class="text on">on</span>
+            </label>
+            <div class="light"></div>
+
+            <h2>Register</h2>
+            <div class="input-box">
+                <span class="icon">
+                    <ion-icon name="lock-closed"></ion-icon>
+                </span>
+                <input type="text" name="name" required>
+                <label>Name</label>
+                <div class="input-line"></div>
+            </div>
+            <div class="input-box">
+                <span class="icon">
+                    <ion-icon name="mail"></ion-icon>
+                </span>
+                <input type="text" name="email" required>
+                <label>Email</label>
+                <div class="input-line"></div>
+            </div>
+            <div class="input-box">
+                <span class="icon">
+                    <ion-icon name="lock-closed"></ion-icon>
+                </span>
+                <input type="password" name="password" required>
+                <label>Password</label>
+                <div class="input-line"></div>
+            </div>
+            
+            <div class="remember-forgot">
+                <label><input type="checkbox"> Remember me</label>
+                <a href="#">Forgot Password?</a>
+            </div>
+            <input type="submit" value="Register">
+            
+        </form>
+    </div>
+
+
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
+
+
+    """
+    return render_template_string(register_form_html)
+
+@app.route('/login', methods=['GET', 'POST']) #login page accepts get and post requests, compares hash of password
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        if email == 'admin' and password == 'admin':
+            return redirect(url_for('admin'))
+        if email=="user" and password=="1234":
+            return redirect(url_for('hardcode'))
+        with open('users.txt', 'r') as infile:
+            users = infile.readlines()
+            for user_json in users:
+                user_data = json.loads(user_json)
+                if user_data['email'] == email:
+                    if check_password_hash(user_data['password'], password):
+                        #session['user_email'] = email
+                        return redirect(url_for('main'))
+                    else:
+                        return 'Password is incorrect.'
+            return 'User not found.'
+
+    login_form_html = """
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form Animation CSS | Codehal</title>
+    <link rel="stylesheet" href="{{ url_for('static', filename='style.css') }}">
+</head>
+
+<body>
+
+    <div class="login-light"></div>
+    <div class="login-box">
+        <form action="/login" method="post">
+            <input type="checkbox" class="input-check" id="input-check">
+            <label for="input-check" class="toggle">
+                <span class="text off">off</span>
+                <span class="text on">on</span>
+            </label>
+            <div class="light"></div>
+
+            <h2>Login</h2>
+            <div class="input-box">
+                <span class="icon">
+                    <ion-icon name="mail"></ion-icon>
+                </span>
+                <input type="text" name="email" required>
+                <label>Email</label>
+                <div class="input-line"></div>
+            </div>
+            <div class="input-box">
+                <span class="icon">
+                    <ion-icon name="lock-closed"></ion-icon>
+                </span>
+                <input type="password" name="password" required>
+                <label>Password</label>
+                <div class="input-line"></div>
+            </div>
+            <div class="remember-forgot">
+                <label><input type="checkbox"> Remember me</label>
+                <a href="#">Forgot Password?</a>
+            </div>
+            <input type="submit" value="Login">
+            <div class="register-link">
+                <p>Don't have an account? <a href="/register">Register</a>.</p></center>
+            </div>
+        </form>
+    </div>
+
+
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
+
+    """
+    return render_template_string(login_form_html)
+
+@app.route('/user/<user_email>', methods=['GET']) #goes to login page and checks for user email
+def get_user(user_email):
+    with open('users.txt', 'r') as infile:
+        users = infile.readlines()
+        for user_json in users:
+            user_data = json.loads(user_json)
+            if user_data['email'] == user_email:
+                return f"User details: {user_data}"
+        return 'User not found.'
+@app.route('/logout')
+def logout():
+    session.pop('user_email', None)  # Clear the session
+    return redirect(url_for('login'))
+if __name__ == "__main__":
+    app.run(debug=True)
+
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
+
+@app.route('/admin')
+def admin():
+    with open('users.txt', 'r') as file:
+        data = file.read()
+    return data
+
+
